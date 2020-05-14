3f64427c6c1093bf57acd1032c491757b3f01b90
Adelar <adelarmello8@gmail.com>
2020-05-13 23:36:16 -0300

    Isolando componentes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,9 @@ import { FiLogIn } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 import { Container, Content, Background } from './styles'
 
 const SignIn: React.FC = () => (
@@ -11,11 +14,11 @@ const SignIn: React.FC = () => (
       <img src={logoImg}  alt="GoBarber" />
       <form>
         <h1> Faca seu login</h1>
-        <input placeholder="E-mail" />
+        <Input name="email" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input name="password"type="password" placeholder="Senha" />
 
-        <button type="submit" >Entrar</button>
+        <Button type="submit" >Entrar</Button>
         <a>Esquecia senha</a>
       </form>
 
