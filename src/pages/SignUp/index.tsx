4a4d8b9e37f4d11d4cf0686481e38c6ad1b0cb26
Adelar <adelarmello8@gmail.com>
2020-06-22 18:10:18 -0300

    Criando tooltip de erros

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -22,7 +22,7 @@ const SignUp: React.FC = () => {
 
       const schema= Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        email: Yup.string().required('Digite um e-mail válido').email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
       await schema.validate(data, {
