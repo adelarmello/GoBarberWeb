442a392e264c5c14ad2ef436b7ca697dfbd6217c
Adelar <adelarmello8@gmail.com>
2020-08-02 17:20:14 -0300

    update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,36 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+import {
+  Container,
+  Content,
+  AnimationContainer,
+  Background,
+} from '../ForgotPassword/styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para o início
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default Dashboard;
+
+import React from 'react';
 
 // import { Container } from './styles';
 
